e2c1f39d0161ea37db24c489c17d8bd96b316192
Ray <12072383+raymondcheung8@users.noreply.github.com>
2020-03-08 01:27:25 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Trees
+namespace Trees2
 {
     /*
      * Tasks:
@@ -20,7 +20,6 @@ namespace Trees
         private Node left;
         private Node right;
         private string item;
-        private Node root;
 
         //Methods
         public Node(string item)
@@ -29,110 +28,103 @@ namespace Trees
             this.right = null;
             this.item = item;
         }
-        public void addNode(string item)
+        public void addNode(string item, Node current)
         {
-            if (root == null) root = this;
+            int compOut = string.Compare(item, current.item);          // Comparison Outcome
+            // -1 means string1 is less than string2
+            // 0 means string 1 is the same as string2
+            // 1 means string 1 is greater than string2
 
-            Node current = root;
-            bool added = false;
-            while (!added)
+            if (compOut == -1 || compOut == 0)
             {
-                int compOut = string.Compare(item, this.item);          // Comparison Outcome
-                // -1 means string1 is less than string2
-                // 0 means string 1 is the same as string2
-                // 1 means string 1 is greater than string2
-
-                if (compOut == -1 || compOut == 0)
-                {
-                    if (this.left != null) current = current.left;
-                    else
-                    {
-                        this.left = new Node(item);
-                        added = true;
-                    }
-                }
-                else if (compOut == 1)
-                {
-                    if (this.right != null) current = current.right;
-                    else
-                    {
-                        this.right = new Node(item);
-                        added = true;
-                    }
-                }
+                if (current.left != null) addNode(item, current.left);
+                else current.left = new Node(item);
+            }
+            else if (compOut == 1)
+            {
+                if (current.right != null) addNode(item, current.right);
+                else current.right = new Node(item);
             }
         }
-        public Boolean findNode(string item)
+        public Boolean findNode(string item, Node current)
         {
-            if (root == null) root = this;
+            int compOut = string.Compare(item, current.item);          // Comparison Outcome
+            // -1 means string1 is less than string2
+            // 0 means string 1 is the same as string2
+            // 1 means string 1 is greater than string2
 
-            Node current = root;
-            bool found = false;
-            int compOut;
-            while (!found)
+            if (compOut == 0) return true;
+            else if (compOut == -1 && current.left != null) return findNode(item, current.left);
+            else if (compOut == 1 && current.right != null) return findNode(item, current.right);
+            return false;
+        }
+        public Boolean deleteNode(string item, Node current)
+        {
+            int compOut = string.Compare(item, current.item);          // Comparison Outcome
+            if (compOut == 0) return true;
+            else if (compOut == -1 && current.left != null)
             {
-                compOut = string.Compare(item, this.item);          // Comparison Outcome
-                // -1 means string1 is less than string2
-                // 0 means string 1 is the same as string2
-                // 1 means string 1 is greater than string2
-
-                if (compOut == 0) found = true;
-                if (compOut == -1)
+                if (deleteNode(item, current.left))
                 {
-                    if (this.left != null) current = current.left;
-                    else break;
-                }
-                else if (compOut == 1)
-                {
-                    if (this.right != null) current = current.right;
-                    else break;
+                    current.left = null;
+                    Console.WriteLine("Successfully Deleted {0}", item);
                 }
             }
-            return found;
-        }
-        public void deleteNote(string item)
-        {
-            if (root == null) root = this;
-
-            Node current = root;
-            Node previous = null;
-            bool found = false;
-            int compOut = -2;
-            int prevCompOut = -2;
-            while (!found)
+            else if (compOut == 1 && current.right != null)
             {
-                prevCompOut = compOut;
-                compOut = string.Compare(item, this.item);          // Comparison Outcome
-                // -1 means string1 is less than string2
-                // 0 means string 1 is the same as string2
-                // 1 means string 1 is greater than string2
-
-                if (compOut == 0) found = true;
-                if (compOut == -1)
+                if (deleteNode(item, current.right))
                 {
-                    if (this.left != null)
-                    {
-                        previous = current;
-                        current = current.left;
-                    }
-                    else break;
-                }
-                else if (compOut == 1)
-                {
-                    if (this.right != null)
-                    {
-                        previous = current;
-                        current = current.right;
-                    }
-                    else break;
+                    current.right = null;
+                    Console.WriteLine("Successfully Deleted {0}", item);
                 }
             }
-
-            if (found && prevCompOut == -1) previous.left = null;
-            else if (found && prevCompOut == 1) previous.right = null;
-            else Console.WriteLine("Item not found!");
+            return false;
         }
-        void printTree() { }
+        public void printTree(string input)
+        {
+            switch (input.ToLower())
+            {
+                case "preorder":
+                    preorder(this);
+                    break;
+                case "inorder":
+                    inorder(this);
+                    break;
+                case "postorder":
+                    postorder(this);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+        private void preorder(Node current)
+        {
+            if (current != null)
+            {
+                Console.WriteLine(current.item);
+                preorder(current.left);
+                preorder(current.right);
+            }
+        }
+        private void inorder(Node current)
+        {
+            if (current != null)
+            {
+                inorder(current.left);
+                Console.WriteLine(current.item);
+                inorder(current.right);
+            }
+        }
+        private void postorder(Node current)
+        {
+            if (current != null)
+            {
+                postorder(current.left);
+                postorder(current.right);
+                Console.WriteLine(current.item);
+            }
+        }
     }
 
     class Program
@@ -151,13 +143,34 @@ namespace Trees
                 if (root == null)
                     root = new Node(mon);
                 else
-                    root.addNode(mon);
+                    root.addNode(mon, root);
             }
 
             // print out the tree using different traversal methods
             //
 
+            root.printTree("");
+            Console.Write("\n");
+            root.printTree("preorder");
+            Console.Write("\n");
+            root.printTree("inorder");
+            Console.Write("\n");
+            root.printTree("postorder");
+            Console.Write("\n");
+
             // Test the findNote() and deleteNode()
+
+            foreach (var mon in months)
+                if (root.findNode(mon, root)) Console.WriteLine("Found {0}", mon);
+
+            Console.Write("\n");
+
+            foreach (var mon in months)
+                root.deleteNode(mon, root);
+            //Can't delete January since it is the root
+            //After deleting Febuary and March, only January will be left, since all the other nodes are connected to Febuary and March
+
+            Console.ReadKey();
         }
     }
 }
